51115fcb11ddc178f13c88cebe111ae21518adbd
Alicia Luna <alicialuna@Alicias-MacBook-Pro.local>
2021-03-14 23:29:08 -0700

    I commit to these changes

--- src/main/java/csc131/junit/GiftcardTest.java
+++ src/main/java/csc131/junit/GiftcardTest.java
@@ -2,6 +2,7 @@ package csc131.junit;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+//import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 //import org.junit.rules.ExpectedException;
@@ -51,8 +52,54 @@ public class GiftcardTest {
 		GiftCard card = new GiftCard(issuingStore,balance);
 		result = card.deduct(amt);
 		amt = 0.00;
-
 		assertEquals("deductBalance()",result,card.deduct(amt));
-	
 	}
+
+	@Test
+	public void deductTest1() {
+		double balance = 100.00;
+		double amt = -5.00;//getAmount();
+		int issuingStore = 1337;
+		GiftCard card = new GiftCard(issuingStore,balance);
+		card.deduct(amt);
+	}
+	
+	@Test
+	public void deductTest2() {
+		double balance = 100.00;
+		double amt = 110;//getAmount();
+		int issuingStore = 1337;
+		GiftCard card = new GiftCard(issuingStore,balance);
+		card.deduct(amt);
+	}
+	
+	@Test
+	public void throwException() {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	@Test
+	public void testExceptions() {	
+		double balance = -5;
+		int issuingStore = 1337;
+		GiftCard card = new GiftCard(issuingStore,balance);
+		card.getBalance();	
+	}
+	
+	@Test
+	public void constructor_IncorrectID_Low() {
+		double balance = 100;
+		int issuingStore = -1;
+		GiftCard card = new GiftCard(issuingStore,balance);
+		card.getIssuingStore();
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High() {
+		double balance = 100;
+		int issuingStore = 100000;
+		GiftCard card = new GiftCard(issuingStore,balance);
+		card.getIssuingStore();
+	}
+
 }
